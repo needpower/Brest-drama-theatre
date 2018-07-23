59df5cb864bf645b00d0a6fb67fafb16b1c3d751
Artyom Lyubchuk <artemlyubchuk@mail.ru>
2018-07-23 17:56:07 +0300

    Refactor theatre events actions using crud library

--- src/Event/actions.js
+++ src/Event/actions.js
@@ -1,69 +1,79 @@
-import { get, post, put, deleteItem } from '../infrastructure/http';
+import cuid from 'cuid';
+import _ from 'lodash';
+import reduxCRUD from 'redux-crud';
+import { deleteItem, get, patch, post } from '../infrastructure/http';
+import TheatreEvent from './model';
 
-export const GET_EVENTS_SUCCESS = 'GetEventsSuccess';
-export const getEventsSuccess = events => ({
-  type: GET_EVENTS_SUCCESS,
-  payload: events,
-});
+const baseActionCreators = reduxCRUD.actionCreatorsFor('events');
 
-export const GET_EVENTS_FAILURE = 'GetEventsFailure';
-export const getEventsFailure = error => ({
-  type: GET_EVENTS_FAILURE,
-  payload: error,
-});
+const {
+  fetchStart,
+  fetchSuccess,
+  fetchError,
+  createStart,
+  createSuccess,
+  createError,
+  updateStart,
+  updateSuccess,
+  updateError,
+  deleteStart,
+  deleteSuccess,
+  deleteError,
+} = baseActionCreators;
 
-export const getEvents = () => dispatch =>
-  get('getEvents')
-    .then(events => dispatch(getEventsSuccess(events)))
-    .catch(error => dispatch(getEventsFailure(error)));
+const eventsActionCreators = {
+  /**
+   * @param {number[]} [ids] If not provided, all events will be fetched
+   */
+  get(ids, replaceExisting = true) {
+    return (dispatch) => {
+      dispatch(fetchStart());
 
-export const CREATE_EVENT_SUCCESS = 'CreateEventSuccess';
-export const createEventSuccess = event => ({
-  type: CREATE_EVENT_SUCCESS,
-  payload: event,
-});
+      return get('getEvents', ids)
+        .then(events => dispatch(fetchSuccess(events, { replace: replaceExisting })))
+        .catch(error => dispatch(fetchError(error)));
+    };
+  },
 
-export const CREATE_EVENT_FAILURE = 'createEventFailure';
-export const createEventFailure = error => ({
-  type: CREATE_EVENT_FAILURE,
-  payload: error,
-});
+  /**
+   * @param {TheatreEvent} event
+   */
+  create(event) {
+    return (dispatch) => {
+      const cid = cuid();
+      // Add new event to store before we get a real created one from server (optimistic creation)
+      const newEvent = {
+        ...event,
+        id: cid,
+      };
 
-export const createEvent = event => dispatch =>
-  post('createEvent', event)
-    .then(createdEvent => dispatch(createEventSuccess(createdEvent)))
-    .catch(error => dispatch(createEventFailure(error)));
+      dispatch(createStart(newEvent));
 
-export const UPDATE_EVENT_SUCCESS = 'updateEventSuccess';
-export const updateEventSuccess = event => ({
-  type: UPDATE_EVENT_SUCCESS,
-  payload: event,
-});
+      return post('createEvent', newEvent)
+        .then(createdEvent => dispatch(createSuccess(createdEvent, cid)))
+        .catch(error => dispatch(createError(error, event)));
+    };
+  },
 
-export const UPDATE_EVENT_FAILURE = 'updateEventFailure';
-export const updateEventFailure = error => ({
-  type: UPDATE_EVENT_FAILURE,
-  payload: error,
-});
+  update(event) {
+    return (dispatch) => {
+      dispatch(updateStart(event));
 
-export const updateEvent = event => dispatch =>
-  put('updateEvent', event)
-    .then(updatedEvent => dispatch(updateEventSuccess(updatedEvent)))
-    .catch(error => dispatch(updateEventFailure(error)));
+      return patch('updateEvent', event)
+        .then(updatedEvent => dispatch(updateSuccess(updatedEvent)))
+        .catch(error => dispatch(updateError(error, event)));
+    };
+  },
 
-export const DELETE_EVENT_SUCCESS = 'deleteEventSuccess';
-export const deleteEventSuccess = eventId => ({
-  type: DELETE_EVENT_SUCCESS,
-  payload: eventId,
-});
+  delete(event) {
+    return (dispatch) => {
+      dispatch(deleteStart(event));
 
-export const DELETE_EVENT_FAILURE = 'deleteEventFailure';
-export const deleteEventFailure = error => ({
-  type: DELETE_EVENT_FAILURE,
-  payload: error,
-});
+      return deleteItem('deleteEvent', event)
+        .then(updatedEvent => dispatch(deleteSuccess(updatedEvent)))
+        .catch(error => dispatch(deleteError(error, event)));
+    };
+  },
+};
 
-export const deleteEvent = eventId => dispatch =>
-  deleteItem('deleteEvent', eventId)
-    .then(deletedEventId => dispatch(deleteEventSuccess(deletedEventId)))
-    .catch(error => dispatch(deleteEventFailure(error)));
+export default _.extend(baseActionCreators, eventsActionCreators);
