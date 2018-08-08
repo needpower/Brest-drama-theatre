2b54d28bd022f20231d0838b830bf0cdd3cd4dba
Artyom Lyubchuk <artemlyubchuk@mail.ru>
2018-08-08 18:13:14 +0300

    Update types in JSDoc

--- src/Event/model.js
+++ src/Event/model.js
@@ -1,21 +1,33 @@
 import { Image } from '../Image/model';
+import { Person } from '../Person/model';
 
-export const MODEL_NAME = 'events';
+/**
+ * @typedef Language
+ * @property {string} English "english"
+ * @property {string} Russian "russian"
+ * @property {string} Belarussian "belarusian"
+ */
+
+/**
+ * @typedef Hall
+ * @property {string} Large "large"
+ * @property {string} Small "small"
+ */
 
 /** Class representing an event
  *
  * @param {Object} event - New event
  * @param {?number} event.ageRestrictions - People younger than this age
  * can't buy tickets for this event
- * @param {string} event.author - An author, director of show
- * @param {string[]} event.characters - People names who play roles in event
+ * @param {Person} event.author - An author, director of show
+ * @param {Person[]} event.characters - People names who play roles in event
  * @param {string} event.description - About event. Can include html, images
  * @param {!number} event.duration - How long an event will take
  * @param {?string} event.genre
- * @param {string} event.hall - In what hall event will occurs
+ * @param {Hall} event.hall - In what hall event will occurs
  * @param {Image[]} images - photo gallery of this event in the past. See {@link Image}
- * @param {string} event.language
- * @param {string} event.poster - A path to cover of event
+ * @param {Language} event.language
+ * @param {Image} event.poster - A path to cover of event
  * @param {?(number[])} event.price. Several prices while no online ticket sale
  * @param {Date} event.start - when event will start
  * @param {!string} event.title - Title of event
--- src/Person/model.js
+++ src/Person/model.js
@@ -1,41 +1,14 @@
-import { List, Record, fromJS } from 'immutable';
+import { Image } from '../Image/model';
 
-const Parent = Record({
-  about: null, // can include html, images. Must be like html container
-  name: '',
-  photos: List(),
-  roles: List(), // can be director, producer and writer at the same time? Or just one role?
-});
+/**
+ * @typedef {Object} Person
+ * @property {?string} about Biography, few words about person
+ * @property {!string} name
+ * @property {Image[]} photos A path to person's photos. See {@link Image}
+ * @property {string[]} roles What person can do in theatre (actor, writer, etc)
+ */
 
 /** Class representing a theatre worker */
-export default class Person extends Parent {
-  /**
-   * Create person
-   *
-   * @param {(Object|string)} person - Who should be created
-   * @param {?string} person.about - Biography, few words about person
-   * @param {!string} person.name
-   * @param {string[]} person.photos - A path to person's photos
-   * @param {string[]} person.roles - What person can do in theatre (actor, writer, etc)
-   */
-  constructor(person) {
-    // An object to be instantiated
-    let immutablePerson = {};
-
-    // Only name was provided
-    if (typeof person === 'string') {
-      immutablePerson = fromJS({ name: person });
-    }
-    // Whole information was provided
-    if (typeof person === 'object') {
-      const photos = fromJS(person.photos);
-      const roles = List(person.roles);
-      immutablePerson = fromJS(Object.assign({}, person, {
-        photos,
-        roles,
-      }));
-    }
-
-    super(immutablePerson);
-  }
+export default function Person(person) {
+  return Object.freeze(person);
 }
