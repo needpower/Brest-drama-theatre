6b83cdbcfa391b90d50d5c4bb38621448edca142
Artyom Lyubchuk <artemlyubchuk@mail.ru>
2018-08-14 16:01:24 +0300

    Person reducer

--- src/Person/reducer.js
+++ src/Person/reducer.js
@@ -1,5 +1,6 @@
 import reduxCRUD from 'redux-crud';
 import { find } from 'lodash';
+import { updateItemInArray } from '../infrastructure/reducer';
 import Person, { MODEL_NAME } from './model';
 import { ADD_PHOTOS, REMOVE_PHOTOS } from './actions';
 
@@ -9,7 +10,16 @@ export default function reducer(state = [], action) {
   switch (action.type) {
     case ADD_PHOTOS: {
       const person = Person(find(state, { id: action.personId }));
+      // @TODO add error protection: if such preson not found
       const personWithAddedPhotos = person.addPhotos(action.photos);
+      return updateItemInArray(state, action.personId, personWithAddedPhotos);
+    }
+
+    case REMOVE_PHOTOS: {
+      const person = Person(find(state, { id: action.personId }));
+      // @TODO add error protection: if such preson not found
+      const personWithRemovedPhotos = person.removePhotos(action.photos);
+      return updateItemInArray(state, action.personId, personWithRemovedPhotos);
     }
 
     default:
--- src/infrastructure/reducer.js
+++ src/infrastructure/reducer.js
@@ -1,6 +1,24 @@
-import { combineReducers } from 'redux-immutable';
+import { combineReducers } from 'redux';
 import routerReducer from './router-reducer';
 import events from '../Event/reducer';
+
+/**
+ * @param {Array} array state
+ * @param {number} itemId item to update
+ * @param {Object} newValues a way to update item
+ */
+export const updateItemInArray = (array, itemId, newValues) => {
+  const updatedItems = array.map((item) => {
+    if (item.id !== itemId) {
+      // Since we only want to update one item, preserve all others that not match
+      return item;
+    }
+    const updatedItem = Object.assign({}, item, newValues);
+    return updatedItem;
+  });
+
+  return updatedItems;
+};
 
 export default combineReducers({
   events,
